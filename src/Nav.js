6cdf7e72fc5e08dc2ca2705a324a06bdb95cf5b6
treklucifer <treklucifershot@gmail.com>
2022-03-15 15:33:17 +0530

    build version

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+       <img
+  className="nav_logo"
+   src="https://upload.wikimedia.org/wikipedia/commons/0/"
+  alt="Netflix Logo"
+  
+/>
+        
+    </div>
+  )
+}
+
+export default Nav
